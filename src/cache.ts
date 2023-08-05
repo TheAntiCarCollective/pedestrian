3306e33db4aece9bdc9cd46d358d6e5d21b33fa1
danthonywalker <thedanthonywalker@gmail.com>
2023-08-05 12:18:44 -0500

    Temporarily Remove Distributed Lock from cache.ts

--- src/cache.ts
+++ src/cache.ts
@@ -1,10 +1,5 @@
-import { v4 as uuid } from "uuid";
-
 import redis from "./services/redis";
 import Environment from "./environment";
-import sleep from "./sleep";
-
-const LOCK_TIMEOUT = 1_000;
 
 // region CacheKey
 const normalizeInput = (value: string) =>
@@ -15,102 +10,38 @@ const keyPrefix = normalizeInput(Environment.PROJECT_NAME);
 export const CacheKey = {
   channel: (channelId: string) => `${keyPrefix}:channel:${channelId}`,
   channels: (query: string) => `${keyPrefix}:channels:${normalizeInput(query)}`,
-  lock: (key: string) => `${keyPrefix}:lock:{${key}}`,
   videos: (playlistId: string) => `${keyPrefix}:videos:${playlistId}`,
 } as const;
 // endregion
 
-const onRejected =
-  <T>(defaultValue: T) =>
-  (error: unknown) => {
-    console.error(error);
-    return defaultValue;
-  };
-
 const get = async <T>(key: string) => {
-  const redisPromise = redis.get(key);
-  const json = await redisPromise.catch(onRejected(null));
+  let json: string | null;
+
+  try {
+    json = await redis.get(key);
+  } catch (error) {
+    console.error(error);
+    json = null;
+  }
 
   if (json === null) return undefined;
   return JSON.parse(json) as T;
 };
 
-const tryLock = async (lockKey: string, lockToken: string) => {
-  const redisPromise = redis.set(lockKey, lockToken, {
-    GET: true,
-    NX: true,
-    PX: LOCK_TIMEOUT,
-  });
-
-  const previousLockToken = await redisPromise.catch(onRejected(null));
-  return previousLockToken === null || previousLockToken === lockToken;
-};
-
-const lock = async (lockKey: string, lockToken: string) => {
-  while (!(await tryLock(lockKey, lockToken))) {
-    const redisPromise = redis.pTTL(lockKey);
-    const untilLockExpires = await redisPromise.catch(onRejected(0));
-    await sleep(untilLockExpires);
-  }
-};
-
-const extendLock = (lockKey: string, lockToken: string) => {
-  const script = `
-    if redis.call("get", KEYS[1]) == ARGV[1] then
-      return redis.call("pexpire", KEYS[1], ARGV[2])
-    else
-      return 0
-    end
-  `;
-
-  const redisPromise = redis.eval(script, {
-    arguments: [lockToken, LOCK_TIMEOUT.toString()],
-    keys: [lockKey],
-  });
-
-  return redisPromise.catch(onRejected(0));
-};
-
-const atomicSet = (
-  lockKey: string,
-  lockToken: string,
+const set = async (
   key: string,
   value: unknown,
   expireInMilliseconds: number,
 ) => {
   const json = JSON.stringify(value);
 
-  const script = `
-    if redis.call("get", KEYS[1]) == ARGV[1] then
-      return redis.call("set", KEYS[2], ARGV[2], "px", ARGV[3])
-    else
-      return nil
-    end
-  `;
-
-  const redisPromise = redis.eval(script, {
-    arguments: [lockToken, json, expireInMilliseconds.toString()],
-    keys: [lockKey, key],
-  });
-
-  return redisPromise.catch(onRejected(null));
-};
-
-const unlock = (lockKey: string, lockToken: string) => {
-  const script = `
-    if redis.call("get", KEYS[1]) == ARGV[1] then
-      return redis.call("del", KEYS[1])
-    else
-      return 0
-    end
-  `;
-
-  const redisPromise = redis.eval(script, {
-    arguments: [lockToken],
-    keys: [lockKey],
-  });
-
-  return redisPromise.catch(onRejected(0));
+  try {
+    await redis.set(key, json, {
+      PX: expireInMilliseconds,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const computeIfAbsent = async <T>(
@@ -118,26 +49,13 @@ const computeIfAbsent = async <T>(
   callback: () => Promise<NonNullable<T>>,
   expireInMilliseconds: number = Number.MAX_VALUE,
 ) => {
-  const lockKey = CacheKey.lock(key);
-  const lockToken = uuid();
-  await lock(lockKey, lockToken);
-
   let value = await get<NonNullable<T>>(key);
 
   if (value === undefined) {
-    const callbackPromise = callback();
-    while (value === undefined) {
-      await extendLock(lockKey, lockToken);
-
-      const sleepPromise = sleep(LOCK_TIMEOUT / 2);
-      const result = await Promise.race([callbackPromise, sleepPromise]);
-      value = result === undefined ? undefined : result;
-    }
-
-    await atomicSet(lockKey, lockToken, key, value, expireInMilliseconds);
+    value = await callback();
+    await set(key, value, expireInMilliseconds);
   }
 
-  await unlock(lockKey, lockToken);
   return value;
 };
 
